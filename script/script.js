65b327e85816540f701d3fc5046484443f4baeb0
Vladimir Yablokov <YablokovWork@gmail.com>
2023-02-10 22:00:24 +0300

    закончена фильтрация массива (поиск) и добавлена валидация на поля ввода

--- script/script.js
+++ script/script.js
@@ -1,38 +1,33 @@
 const addFormElem = document.forms[0];
-const searchFormElem = document.forms[1]
+const searchFormElem = document.forms[1];
+// const sElem = searchFormElem.search;
 const resultElem = document.querySelector('#result');
-const upSortElem = document.querySelector('.sortUp')
-const downSortElem = document.querySelector('.sortDown')
-let cards = ['Vova', 'Yablokov', 'learn', 'Java', 'script'];
-
+const upSortElem = document.querySelector('.sortUp');
+const downSortElem = document.querySelector('.sortDown');
+let cards = ['2', 'Vladimir', 'Yablokov', '3', 'learn', 'Java', 'script', '1'];
 
 addFormElem.addEventListener('submit', event => {
     event.preventDefault();
-    const name = event.target.title.value;
-    cards.push(name)
-    render()
-    event.target.reset()
-})
+    if (addFormElem.title.value === '') {
+        alert('введите название')
+    } else {
+        const name = event.target.title.value;
+        cards.push(name);
+        render();
+        event.target.reset();
+    };
+});
 
-searchFormElem.addEventListener('submit', event=>{
+searchFormElem.addEventListener('submit', event => {
     event.preventDefault();
-    const searchName = event.target.search.value;
-    // console.log(searchName);
-    cards.filter(searchName => searchName.name === cards.name)
-    render()
-})
-
-function render() {
-    resultElem.innerText = '';
-    for (let i = 0; i < cards.length; i++) {
-        let name = cards[i]
-        const card = document.createElement('div');
-        const nameElem = document.createElement('p');
-        resultElem.append(card);
-        card.append(nameElem);
-        nameElem.innerText = name
-    }
-}
+    if (searchFormElem.search.value === '') {
+        alert('введите значение поиска')
+    } else {
+        cards = cards.filter(elem => elem === searchFormElem.search.value);
+        render();
+        event.target.reset();
+    };
+});
 
 upSortElem.addEventListener('click', event => {
     cards.sort((a, b) => {
@@ -45,7 +40,7 @@ upSortElem.addEventListener('click', event => {
         return 0;
     });
     render();
-})
+});
 
 downSortElem.addEventListener('click', event => {
     cards.sort((a, b) => {
@@ -58,6 +53,17 @@ downSortElem.addEventListener('click', event => {
         return 0;
     });
     render();
-})
+});
+render();
 
-render()
+function render() {
+    resultElem.innerText = '';
+    for (let i = 0; i < cards.length; i++) {
+        let name = cards[i]
+        const card = document.createElement('div');
+        const nameElem = document.createElement('p');
+        resultElem.append(card);
+        card.append(nameElem);
+        nameElem.innerText = name;
+    };
+};
