e29a31667fa04caf2ae96bb62e291cec948a3e25
Vladimir Yablokov <YablokovWork@gmail.com>
2023-02-10 19:14:29 +0300

    начал делать сортировку массива с выводом в карточки, недоделал=)

--- script/script.js
+++ script/script.js
@@ -1,17 +1,25 @@
-const formElem = document.forms[0];
+const addFormElem = document.forms[0];
+const searchFormElem = document.forms[1]
 const resultElem = document.querySelector('#result');
 const upSortElem = document.querySelector('.sortUp')
 const downSortElem = document.querySelector('.sortDown')
-let cards = ['a', 'W', 'c', 'z', 'B', ];
+let cards = ['Vova', 'Yablokov', 'learn', 'Java', 'script'];
 
 
-formElem.addEventListener('submit', event => {
+addFormElem.addEventListener('submit', event => {
     event.preventDefault();
-    const name = event.target.title.value
+    const name = event.target.title.value;
     cards.push(name)
     render()
-
     event.target.reset()
+})
+
+searchFormElem.addEventListener('submit', event=>{
+    event.preventDefault();
+    const searchName = event.target.search.value;
+    // console.log(searchName);
+    cards.filter(searchName => searchName.name === cards.name)
+    render()
 })
 
 function render() {
@@ -23,7 +31,6 @@ function render() {
         resultElem.append(card);
         card.append(nameElem);
         nameElem.innerText = name
-        nameElem.value = ''
     }
 }
 
