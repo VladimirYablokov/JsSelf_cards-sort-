43bcb2cca2b21ffc0952e71600b15251f5c64039
Vladimir Yablokov <YablokovWork@gmail.com>
2023-02-11 22:24:51 +0300

    закончил сортировку, добавил сортировку по статусу, обрнул все действия в функции

--- script/script.js
+++ script/script.js
@@ -1,10 +1,15 @@
 const addFormElem = document.forms[0];
 const searchFormElem = document.forms[1];
+
 const resultElem = document.querySelector('#result');
-const upSortElem = document.querySelector('.sortUp');
-const downSortElem = document.querySelector('.sortDown');
-let cards = [
-    {
+
+const upAlphabetSortElem = document.querySelector('.sortAlphabetUp');
+const downAlphabetSortElem = document.querySelector('.sortAlphabetDown');
+
+const upStatusSortElem = document.querySelector('.sortStatusUp');
+const downStatusSortElem = document.querySelector('.sortStatusDown');
+
+let cards = [{
         title: 'Vova',
         status: '1'
     },
@@ -31,62 +36,66 @@ function render(list) {
     for (let elem of list) {
         const card = document.createElement('div');
         const nameElem = document.createElement('p');
+        const statusElem = document.createElement('p')
         resultElem.append(card);
-        card.append(nameElem);
-        nameElem.innerText = `${elem.title} (${elem.status})`;
+        card.append(nameElem, statusElem);
+        nameElem.innerText = `${elem.title}`;
+        statusElem.innerText = `Status: ${elem.status}`
     };
 };
+render(cards);
 
-addFormElem.addEventListener('submit', event => {
-    event.preventDefault();
+function formAdd() {
+    addFormElem.addEventListener('submit', event => {
+        event.preventDefault();
         const title = event.target.title.value;
         const status = event.target.status.value;
-        if(title !== '' && status !== ''){
-            cards.push({title, status});
-        }else{
+        if (title !== '' && status !== '') {
+            cards.push({
+                title,
+                status
+            });
+        } else {
             alert('одно из полей пустое!')
         }
         event.target.title.value = '';
         event.target.status.value = ''
         render(cards);
-});
+    });
+}
+formAdd()
 
-searchFormElem.addEventListener('submit', event => {
-    event.preventDefault();
-    if (searchFormElem.search.value === '') {
-        alert('введите значение поиска')
-    } else {
-        const value = event.target.search.value
-        const lst = cards.filter(elem => elem.title.startsWith(value))
-        event.target.search.value = ''
-        render(lst);
-    };
-});
+function formSearch() {
+    searchFormElem.addEventListener('submit', event => {
+        event.preventDefault();
+        if (searchFormElem.search.value === '') {
+            alert('введите значение поиска');
+        } else {
+            const value = event.target.search.value;
+            const lst = cards.filter(elem => elem.title.startsWith(value));
+            event.target.search.value = '';
+            render(lst);
+        };
+    });
+}
+formSearch()
 
-// upSortElem.addEventListener('click', event => {
-//     cards.sort((a, b) => {
-//         if (a.toLowerCase() < b.toLowerCase()) {
-//             return -1;
-//         }
-//         if (a.toLowerCase() > b.toLowerCase()) {
-//             return 1;
-//         }
-//         return 0;
-//     });
-//     render();
-// });
-
-// downSortElem.addEventListener('click', event => {
-//     cards.sort((a, b) => {
-//         if (a.toLowerCase() > b.toLowerCase()) {
-//             return -1;
-//         }
-//         if (a.toLowerCase() < b.toLowerCase()) {
-//             return 1;
-//         }
-//         return 0;
-//     });
-//     render();
-// });
-render(cards);
-
+function sort() {
+    upAlphabetSortElem.addEventListener('click', event => {
+        cards.sort((value1, value2) => value1.title.localeCompare(value2.title))
+        render(cards);
+    });
+    downAlphabetSortElem.addEventListener('click', event => {
+        cards.sort((value1, value2) => value2.title.localeCompare(value1.title))
+        render(cards);
+    });
+    upStatusSortElem.addEventListener('click', event => {
+        cards.sort((value1, value2) => value1.status.localeCompare(value2.status))
+        render(cards);
+    });
+    downStatusSortElem.addEventListener('click', event => {
+        cards.sort((value1, value2) => value2.status.localeCompare(value1.status))
+        render(cards);
+    });
+}
+sort()
