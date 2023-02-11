fd9002e85200238b005fbac16a0bd3a1b041f89b
Vladimir Yablokov <YablokovWork@gmail.com>
2023-02-11 18:20:51 +0300

    добавил обработку статуса, вместо массива добавил объект, переписал функцию рендера и союытия под обработку объекта(осталось доработать сортировку по наименованию и добавить по статусу)

--- script/script.js
+++ script/script.js
@@ -3,18 +3,52 @@ const searchFormElem = document.forms[1];
 const resultElem = document.querySelector('#result');
 const upSortElem = document.querySelector('.sortUp');
 const downSortElem = document.querySelector('.sortDown');
-let cards = ['2', 'Vladimir', 'Yablokov', '3', 'learn', 'Java', 'script', '1'];
+let cards = [
+    {
+        title: 'Vova',
+        status: '1'
+    },
+    {
+        title: 'Yablokov',
+        status: '2'
+    },
+    {
+        title: 'learn',
+        status: '3'
+    },
+    {
+        title: 'Java',
+        status: '4'
+    },
+    {
+        title: 'script',
+        status: '5'
+    }
+];
+
+function render(list) {
+    resultElem.innerText = '';
+    for (let elem of list) {
+        const card = document.createElement('div');
+        const nameElem = document.createElement('p');
+        resultElem.append(card);
+        card.append(nameElem);
+        nameElem.innerText = `${elem.title} (${elem.status})`;
+    };
+};
 
 addFormElem.addEventListener('submit', event => {
     event.preventDefault();
-    if (addFormElem.title.value === '') {
-        alert('введите название')
-    } else {
-        const name = event.target.title.value;
-        cards.push(name);
-        render();
-        event.target.reset();
-    };
+        const title = event.target.title.value;
+        const status = event.target.status.value;
+        if(title !== '' && status !== ''){
+            cards.push({title, status});
+        }else{
+            alert('одно из полей пустое!')
+        }
+        event.target.title.value = '';
+        event.target.status.value = ''
+        render(cards);
 });
 
 searchFormElem.addEventListener('submit', event => {
@@ -22,47 +56,37 @@ searchFormElem.addEventListener('submit', event => {
     if (searchFormElem.search.value === '') {
         alert('введите значение поиска')
     } else {
-        cards = cards.filter(elem => elem === searchFormElem.search.value);
-        render();
-        event.target.reset();
+        const value = event.target.search.value
+        const lst = cards.filter(elem => elem.title.startsWith(value))
+        event.target.search.value = ''
+        render(lst);
     };
 });
 
-upSortElem.addEventListener('click', event => {
-    cards.sort((a, b) => {
-        if (a.toLowerCase() < b.toLowerCase()) {
-            return -1;
-        }
-        if (a.toLowerCase() > b.toLowerCase()) {
-            return 1;
-        }
-        return 0;
-    });
-    render();
-});
+// upSortElem.addEventListener('click', event => {
+//     cards.sort((a, b) => {
+//         if (a.toLowerCase() < b.toLowerCase()) {
+//             return -1;
+//         }
+//         if (a.toLowerCase() > b.toLowerCase()) {
+//             return 1;
+//         }
+//         return 0;
+//     });
+//     render();
+// });
 
-downSortElem.addEventListener('click', event => {
-    cards.sort((a, b) => {
-        if (a.toLowerCase() > b.toLowerCase()) {
-            return -1;
-        }
-        if (a.toLowerCase() < b.toLowerCase()) {
-            return 1;
-        }
-        return 0;
-    });
-    render();
-});
-render();
+// downSortElem.addEventListener('click', event => {
+//     cards.sort((a, b) => {
+//         if (a.toLowerCase() > b.toLowerCase()) {
+//             return -1;
+//         }
+//         if (a.toLowerCase() < b.toLowerCase()) {
+//             return 1;
+//         }
+//         return 0;
+//     });
+//     render();
+// });
+render(cards);
 
-function render() {
-    resultElem.innerText = '';
-    for (let i = 0; i < cards.length; i++) {
-        let name = cards[i]
-        const card = document.createElement('div');
-        const nameElem = document.createElement('p');
-        resultElem.append(card);
-        card.append(nameElem);
-        nameElem.innerText = name;
-    };
-};
